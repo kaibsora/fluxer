42d6e2752c69a7768fd106c443e20905518a3f6e
Kai Compton <kaibsora@gmail.com>
2026-07-17 08:15:45 -0400

    updating entroypoint for gateway

--- fluxer_gateway/scripts/docker_entrypoint.sh
+++ fluxer_gateway/scripts/docker_entrypoint.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env sh
+
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+set -eu
+
+: "${FLUXER_ERLANG_NODE_NAME:=fluxer_gateway@127.0.0.1}"
+: "${FLUXER_ERLANG_COOKIE:=fluxer_gateway_dev_cookie}"
+: "${FLUXER_ERLANG_DIST_PORT:=8081}"
+
+export FLUXER_ERLANG_NODE_NAME
+export FLUXER_ERLANG_COOKIE
+export FLUXER_ERLANG_DIST_PORT
+export RELX_REPLACE_OS_VARS="${RELX_REPLACE_OS_VARS:-true}"
+
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
@@ -38,17 +53,6 @@ file_env 'FLUXER_ADMIN_SECRET_KEY_BASE'
 file_env 'FLUXER_ADMIN_OAUTH_CLIENT_SECRET'  
 file_env 'MEILI_MASTER_KEY'
 
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-set -eu
-
-: "${FLUXER_ERLANG_NODE_NAME:=fluxer_gateway@127.0.0.1}"
-: "${FLUXER_ERLANG_COOKIE:=fluxer_gateway_dev_cookie}"
-: "${FLUXER_ERLANG_DIST_PORT:=8081}"
-
-export FLUXER_ERLANG_NODE_NAME
-export FLUXER_ERLANG_COOKIE
-export FLUXER_ERLANG_DIST_PORT
-export RELX_REPLACE_OS_VARS="${RELX_REPLACE_OS_VARS:-true}"
+exec "$@"
 
 exec /opt/fluxer_gateway/bin/fluxer_gateway foreground >/dev/null 2>&1
